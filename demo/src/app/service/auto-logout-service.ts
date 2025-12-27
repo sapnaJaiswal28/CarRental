dec8efb93c5584e94c3282d29faeff7deb1f1418
sapnaJaiswal28 <Jsapana774@gmail.com>
2025-12-27 12:10:43 +0530

    timout fix for inactive session

--- demo/src/app/service/auto-logout-service.ts
+++ demo/src/app/service/auto-logout-service.ts
@@ -2,11 +2,13 @@ import { Injectable, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AutoLogoutService {
   timeoutId: any;
-  timeoutDuration = 2 * 60 * 1000; // 2 minutes
+  warningId: any;
+  warningDuration = 2 * 60 * 1000; // show alert at 4 minutes (1 min before logout)
+  timeoutDuration = 3 * 60 * 1000; // 5 minutes
 
   constructor(private router: Router, private ngZone: NgZone) {
     this.initListener();
@@ -14,14 +16,29 @@ export class AutoLogoutService {
   }
 
   initListener() {
-    ['click', 'mousemove', 'keypress', 'scroll'].forEach(event => {
+    ['click', 'mousemove', 'keypress', 'scroll'].forEach((event) => {
       window.addEventListener(event, () => this.resetTimer());
     });
   }
 
   resetTimer() {
     clearTimeout(this.timeoutId);
+    clearTimeout(this.warningId);
+
+    // schedule warning
+    this.warningId = setTimeout(() => this.showWarning(), this.warningDuration);
+
+    // schedule logout
     this.timeoutId = setTimeout(() => this.logout(), this.timeoutDuration);
+  }
+
+  showWarning() {
+    const stayLoggedIn = confirm(
+      'You will be logged out in 1 minute due to inactivity. Do you want to stay logged in?'
+    );
+    if (stayLoggedIn) {
+      this.resetTimer(); // reset timers if user chooses to stay
+    }
   }
 
   logout() {
